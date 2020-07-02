0fafa9b083d65565d8cb50e3b54d1829d27a17f9
xfnw <xfnw@ttm.sh>
2020-07-02 01:14:21 +0000

    radiobot rip

--- modules/radio.py
+++ modules/radio.py
@@ -64,6 +64,60 @@ def timeDelta(dt):
           return str(int(years)) + " years to go"
 
 
+
+
+
+def formatSec(dt):
+
+    delta = dt
+    minutes = delta / MINUTE
+    hours = delta / HOUR
+    days = delta / DAY
+
+    if delta <  0:
+        return "unknown amount of time"
+
+    if delta < 1 * MINUTE:    
+      if delta == 1:
+          return  "one second"
+      else:
+          return str(delta) + " seconds"
+
+
+    if delta < 2 * MINUTE:
+        return "a minute"
+
+
+    if delta < 45 * MINUTE:
+        return str(int(minutes)) + " minutes"
+
+    if delta < 90 * MINUTE:
+        return "one hour"
+
+    if delta < 24 * HOUR:
+        return str(int(hours)) + " hours"
+
+    if delta < 48 * HOUR:
+        return "a day"
+
+    if delta < 30 * DAY:
+        return str(int(days)) + " days"
+
+
+    if delta < 12 * MONTH:
+        months = delta / MONTH
+        if months <= 1:
+            return "one month"
+        else:
+            return str(int(months)) + " months"
+    else:
+      years = days / 365.0
+      if  years <= 1:
+          return "one year"
+      else:
+          return str(int(years)) + " years"
+
+
 async def upnext(self,c,n,m):
     res = requests.get('https://radio.tildeverse.org/api/station/1/schedule')
     if res.status_code == 200:
@@ -76,9 +130,27 @@ async def upnext(self,c,n,m):
     else:
         await self.message(c,'[\x036radio\x0f] something went wrong...')
 
+async def nowplaying(self,c,n,m):
+    res = requests.get("https://radio.tildeverse.org/api/nowplaying/1")
+    if res.status_code == 200:
+        js = res.json()
+        if "station" not in js:
+            await self.message(c,'[\x036radio\x0f] something went wrong...')
+            return
+        np = js['now_playing']
+        #print(np)
+        if np['streamer'] == "":
+            await self.message(c,'[\x036radio\x0f] autodj has been playing {} for {} (next song in {})'.format(np['song']['text'],formatSec(np['elapsed']),formatSec(np['remaining']-1)))
+        else:
+            await self.message(c,'[\x036radio\x0f] {} has been playing "{}" for {} (next song in {})'.format(np['streamer'],np['song']['text'],formatSec(np['elapsed']),formatSec(np['remaining']-1)))
+    else:
+        await self.message(c,'[\x036radio\x0f] something went wrong...')
+
+
 async def init(self):
     self.cmd['un'] = upnext
     self.cmd['upnext'] = upnext
     self.help['upnext'] = ['upnext - get who will be up next on tilderadio\'s schedule','noice moosic']
-
+    self.cmd['nowplaying'] = nowplaying
+    self.help['nowplaying'] = ['nowplaying - when radiobot is dead use this instead!','lol']
 
