5d8cfa59a13e1fbe0942264c952b846d81ef6fdb
xfnw <xfnw@ttm.sh>
2020-08-14 13:37:03 -0400

    simple spam prevention

---
+++ modules/prevention.py
@@ -0,0 +1,35 @@
+
+import random
+
+async def plogger(self,c,n,m):
+  if c not in self.plog:
+    self.plog[c] = []
+
+  self.plog[c].append([n,m])
+  if len(self.plog[c]) > 50:
+    del self.plog[c][:-50]
+
+
+  if c in self.channels and 'o' in self.channels[c]['modes'] and self.nickname in self.channels[c]['modes']['o']:
+      # fun time
+      umc = len([i for i in self.plog[c][-10:] if i[0]==n])
+      #await self.message(c,str(umc))
+      if umc > 6:
+          if n in self.wlevel:
+              self.wlevel[n] += 1
+          else:
+              self.wlevel[n] = 0
+          if self.wlevel[n] == 3:
+              await self.message(c,'[\x036prevention\x0f] hey {}, cool it with all the messages'.format(n))
+          if self.wlevel[n] > 6:
+              self.wlevel[n] = 0
+              await self.set_mode(c,self.mutesyntax[0],self.mutesyntax[1].format(n+'!*@*'))
+  
+
+
+async def init(self):
+  self.plog = {}
+  self.wlevel = {}
+  self.mutesyntax = ['+b','m:{}'] # ['+q','{}'] on freenode
+  self.raw['preventionlog'] = plogger
+
